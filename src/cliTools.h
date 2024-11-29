3103476823ea3396ff431e612fa859432a3ef621
chronos <processor.dev1l@gmail.com>
2024-11-29 19:25:55 +0100

    Added readKey to header
    Added function readKey to `cliTools.h` header file.

--- src/cliTools.h
+++ src/cliTools.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <array>
+#include <cstdint>
 #include<string_view>
 
 struct ParamValueError {
@@ -21,6 +23,8 @@ struct CmdParameters {
 std::string_view getOptionValue(int argc, int& index, char** argv);
 CmdParameters parseArguments(int argc, char** argv);
 bool validateParams(const CmdParameters& params);
+
 bool writeSymmetricKey(std::string_view filePath);
 bool writeSymmetricKey(std::string_view filePath, std::string_view passphrase);
 
+bool readKey(std::string_view filePath, std::array<uint8_t, 256>& outputKey);
